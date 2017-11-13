96bf500904be54cb748e3fcd901a4a4a73faffa3
iefhanz <ziefhanz@gmail.com>
2017-11-13 10:42:09 +0700

    Initial commit

---
+++ app/src/main/java/iefhanz/sqlite/MainActivity.java
@@ -0,0 +1,30 @@
+package iefhanz.sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name,college;
+    MyCoreDatabase myData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = (EditText) findViewById(R.id.edit1);
+        college = (EditText) findViewById(R.id.edit2);
+
+        myData = new MyCoreDatabase(this);
+    }
+
+    public void doSave(View view) {
+        myData.insertData(name.getText().toString(),college.getText().toString());
+    }
+
+    public void doLoad(View view) {
+        myData.getAll();
+    }
+}
